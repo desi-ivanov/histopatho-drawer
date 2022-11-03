b281ee040e9bd8f1113e93aa58a4c5f47cae3cdb
evolveyourmind <20824840+desi-ivanov@users.noreply.github.com>
2022-11-03 10:40:53 +0100

    Remove code

--- server/docker_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -eou pipefail
-
-# Build the docker image
-docker build -t histopatho-drawer-server . --platform linux/amd64
--- server/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -eou pipefail
-FLASK_APP=main.py python -m flask run --host=0.0.0.0 --port=8080
